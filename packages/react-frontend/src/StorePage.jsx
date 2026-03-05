5f86366883b3dee6c1da00b185939da9febd7f6d
Jason Huynh <jhbeautysec@gmail.com>
2026-03-05 00:54:42 -0800

    Fixxed API_URL

--- packages/react-frontend/src/StorePage.jsx
+++ packages/react-frontend/src/StorePage.jsx
@@ -19,7 +19,7 @@ import "./StorePage.css";
 
 //Switch to azure when live
 const API_URL =
-  "https://polyratemyfood-ezfxgaf9dcgf9dcgpdkga.eastus-01.azurewebsites.net";
+  "https://polyratemyfood-ezfxgaf9dcgpdkga.eastus-01.azurewebsites.net";
 
 const BANNER =
   "https://ssgse.com/ssg/wp-content/uploads/SSG-CalPoly-CampusMktUU-2-1024x563.jpg";
